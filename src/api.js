72958f8ff83ecd8d22ba2570701efef326a7723d
shreekant94 <62957881+shreekant94@users.noreply.github.com>
2024-12-10 12:29:38 +0530

    changed origin from local to server

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // Update with your backend URL
+const API_BASE_URL = "https://mytodos-api.onrender.com/api"; // Update with your backend URL
 
 export const getTasks = async () => {
   const response = await axios.get(`${API_BASE_URL}/tasks`);
